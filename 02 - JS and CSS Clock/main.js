224e561db79b1ff4ea658fb33778e399e29d98d6
Hector Vasquez <hvasquezdev@gmail.com>
2019-01-14 20:29:30 -0400

    Fix animation bump when clock hands hit zero

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -26,16 +26,23 @@ function setDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const secondsDegrees = ((seconds / 60) * 360) + 360;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+  const minsDegrees = ((mins / 60) * 360) + 360;
   minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 12) * 360) + ((mins / 60) * 30) + 90;
+  const hoursDegrees = ((hours / 12) * 360) + 360;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  // fix for animation bump on when clock hands hit zero
+  if (secondsDegrees === 714 || secondsDegrees === 360) {
+    secondHand.style.transition = "all 0s ease 0s";
+  } else {
+    secondHand.style.transition = "all 0.05s cubic-bezier(0, 0, 0.52, 2.51) 0s";
+  }
 
   setDigitalDate(now);
 }
