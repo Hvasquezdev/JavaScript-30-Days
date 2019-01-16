92e55feb8be66ae32f1533469aeaa50a55de3708
Hector Vasquez <hvasquezdev@gmail.com>
2019-01-15 23:43:54 -0400

    Update CSS Variables with Js

---
+++ 03 - CSS Variables/main.js
@@ -0,0 +1,9 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleInput() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, `${this.value}${suffix}`);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleInput));
+inputs.forEach(input => input.addEventListener('mousemove', handleInput));
